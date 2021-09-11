c97f4fe57e23dc256a0a997fa1cecb0c044f3e77
Sol Thiessen <soljthiessen@gmail.com>
2021-09-11 16:19:08 -0700

    Added click, correct, and wrong sounds and favicon

--- dropdown.js
+++ dropdown.js
@@ -1,9 +1,15 @@
+clickSound = new Audio('sounds/sounds_click.mp3');
+correctSound = new Audio('sounds/sounds_correct.mp3');
+wrongSound = new Audio('sounds/sounds_wrong.mp3');
+alertSound = new Audio('sounds/sounds_alert.mp3');
+
 function init() {
     //alert("Alert box!")
 }
 
 function check(rownumber){
     //alert("checking response for row " + rownumber);
+    clickSound.play()
     const select = document.getElementById("select" + rownumber);
     const value = select.options[select.selectedIndex].value;
     const btn = document.getElementById("checkbtn" + rownumber);
@@ -12,14 +18,17 @@ function check(rownumber){
     btn.style.display = "none";
     if(value === "correct") {
         correct.style.display = "block";
+        correctSound.play();
     } else {
         wrong.style.display = "block";
+        wrongSound.play();
     }
         //alert("you got it right!");
     
 }
 
 function reset(){
+    clickSound.play()
     for (let index = 1; index < 5; index++) {
         resetRow(index);
         document.getElementById("select" + index.toString()).selectedIndex = 0;
