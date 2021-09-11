c82cae9393b12b65bfa61d8f483d8b5af1374b46
Sol Thiessen <soljthiessen@gmail.com>
2021-09-11 15:40:14 -0700

    Added reset button. Check button reappears after changing selection

--- dropdown.js
+++ dropdown.js
@@ -1,6 +1,7 @@
 function init() {
     //alert("Alert box!")
 }
+
 function check(rownumber){
     //alert("checking response for row " + rownumber);
     const select = document.getElementById("select" + rownumber);
@@ -10,6 +11,29 @@ function check(rownumber){
     if(value === "correct") {
         btn.style.display = "none";
         correct.style.display = "block";
+        for (let index = 0; index < select.options.length; index++) {
+            if(select.options[index].value != "correct")
+                document.removeChild(select.options[index]);
+        }
         //alert("you got it right!");
     }
+}
+
+function reset(){
+    for (let index = 1; index < 5; index++) {
+        resetRow(index);
+        document.getElementById("select" + index.toString()).selectedIndex = 0;
+    }
+}
+
+function resetRow(rownumber){
+    document.getElementById("checkbtn" + rownumber.toString()).style.display = "table-cell";
+    document.getElementById("correct" + rownumber.toString()).style.display = "none";
+}
+
+function changedSelect(rownumber){
+    const select = document.getElementById("select" + rownumber.toString());
+    const value = select.options[select.selectedIndex].value;
+    if(value != "correct")
+        resetRow(rownumber);
 }
